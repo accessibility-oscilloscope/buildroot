93c3a6dd8f3a24a23bad7877e6600810cd099501
jleightcap <jack@leightcap.com>
2022-03-04 00:39:05 -0500

    packaged drv2605 driver + pnt-lut

---
+++ accessibility-oscilloscope/package/drv2605-clean/drv2605-clean.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# drv2605-clean
+#
+################################################################################
+
+DRV2605_CLEAN_VERSION = v0.1
+DRV2605_CLEAN_SOURCE = $(DRV2605_CLEAN_VERSION).tar.gz
+DRV2605_CLEAN_SITE = https://github.com/accessibility-oscilloscope/drv2605-clean/archive/refs/tags/
+
+define DRV2605_CLEAN_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) haptic-driver
+endef
+
+define DRV2605_CLEAN_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/haptic-driver $(TARGET_DIR)/usr/bin/haptic-driver
+endef
+
+$(eval $(generic-package))
---
+++ accessibility-oscilloscope/package/pnt-lut/pnt-lut.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# pnt-lut
+#
+################################################################################
+
+PNT_LUT_VERSION = v0.0.1
+PNT_LUT_SOURCE = $(PNT_LUT_VERSION).tar.gz
+PNT_LUT_SITE = https://github.com/accessibility-oscilloscope/pnt-lut/archive/refs/tags
+
+define PNT_LUT_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) pnt-lut
+endef
+
+define PNT_LUT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/pnt-lut $(TARGET_DIR)/usr/bin/pnt-lut
+endef
+
+$(eval $(generic-package))
