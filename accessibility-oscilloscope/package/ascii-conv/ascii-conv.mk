b1a1052b1859d3e1025a42d838f95c051aef848a
Nick Fantasia <fantasia.n@northeastern.edu>
2022-03-09 10:15:51 -0500

    Added ascii-conv to init.d

--- accessibility-oscilloscope/package/ascii-conv/ascii-conv.mk
+++ accessibility-oscilloscope/package/ascii-conv/ascii-conv.mk
@@ -6,7 +6,7 @@
 
 ASCII_CONV_VERSION = v0.0.1
 ASCII_CONV_SOURCE = $(ASCII_CONV_VERSION).tar.gz
-ASCII_CONV_SITE = https://github.com/accessibility-oscilloscope/ascii-conv/archive/tags
+ASCII_CONV_SITE = https://github.com/accessibility-oscilloscope/ascii-conv/archive/tags/
 
 define ASCII_CONV_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) ascii-conv
