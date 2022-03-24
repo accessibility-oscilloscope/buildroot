d74a9fed8f6445e3d2c8151452ab3fefcc3e0678
jleightcap <jack@leightcap.com>
2022-03-23 22:27:21 -0400

    package installation versioning in /usr/bin

--- accessibility-oscilloscope/package/pnt-lut/pnt-lut.mk
+++ accessibility-oscilloscope/package/pnt-lut/pnt-lut.mk
@@ -8,12 +8,15 @@ PNT_LUT_VERSION = v0.0.10
 PNT_LUT_SOURCE = $(PNT_LUT_VERSION).tar.gz
 PNT_LUT_SITE = https://github.com/accessibility-oscilloscope/pnt-lut/archive/refs/tags
 
+PNT_LUT_BIN = pnt-lut
+
 define PNT_LUT_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) pnt-lut
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) $(PNT_LUT_BIN)
 endef
 
 define PNT_LUT_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/pnt-lut $(TARGET_DIR)/usr/bin/pnt-lut
+	$(INSTALL) -D -m 0755 $(@D)/$(PNT_LUT_BIN) $(TARGET_DIR)/usr/bin/$(PNT_LUT_BIN)-$(PNT_LUT_VERSION)
+	ln -f $(TARGET_DIR)/usr/bin/$(PNT_LUT_BIN)-$(PNT_LUT_VERSION) $(TARGET_DIR)/usr/bin/$(PNT_LUT_BIN)
 endef
 
 $(eval $(generic-package))
